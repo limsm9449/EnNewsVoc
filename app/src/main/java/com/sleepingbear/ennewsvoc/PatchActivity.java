9fb3d20fe556172e8d7fb4d87f7e8205eef221ac
limsm9449 <limsm9449@naver.com>
2016-12-29 10:44:32 +0900

    * 2016.12.28      기능 수정

--- app/src/main/java/com/sleepingbear/ennewsvoc/PatchActivity.java
+++ app/src/main/java/com/sleepingbear/ennewsvoc/PatchActivity.java
@@ -29,15 +29,13 @@ public class PatchActivity extends AppCompatActivity {
 
         StringBuffer patch = new StringBuffer();
 
-        patch.append("* 2016.12.28" + CommConstants.sqlCR);
+        patch.append("* 신규 패치" + CommConstants.sqlCR);
         patch.append("" + CommConstants.sqlCR);
         patch.append("- 단어장에서 선택을 해서 삭제하거나, 다른 단어장으로 복사, 이동하는 기능 추가" + CommConstants.sqlCR);
         patch.append("" + CommConstants.sqlCR);
         patch.append("" + CommConstants.sqlCR);
 
-        patch.append("* 2016.11.21" + CommConstants.sqlCR);
-        patch.append("" + CommConstants.sqlCR);
-        patch.append("- 영어뉴스 어플 개발" + CommConstants.sqlCR);
+        patch.append("- 2016.11.21 : 영어뉴스 어플 개발" + CommConstants.sqlCR);
 
         ((TextView) this.findViewById(R.id.my_c_patch_tv1)).setText(patch.toString());
     }
